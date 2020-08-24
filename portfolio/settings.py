cfd69f22af7c2985993ba155b0bef48f254c61d2
Kazemy <a.kazemy@gmail.com>
2020-08-24 17:59:44 -0400

    Jobs and Blog apps are up and running

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'jobsdb',
+        'USER':'postgres',
+        'PASSWORD':'admin',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
@@ -118,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+#go to media folder; then create images folder; and the save image there
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#to access that image go to our website then /media/images/...
+MEDIA_URL = '/media/'
